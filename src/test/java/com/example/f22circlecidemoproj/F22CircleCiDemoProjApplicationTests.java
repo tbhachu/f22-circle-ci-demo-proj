8bbd8f81a447cbe466075b655de9442074fad4d7
tbhachu <sem.b1901@gmail.com>
2022-12-01 11:39:28 -0800

    Added initial files.

---
+++ src/test/java/com/example/f22circlecidemoproj/F22CircleCiDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.f22circlecidemoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F22CircleCiDemoProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
